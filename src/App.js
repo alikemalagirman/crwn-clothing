f3903cf17316d73a77826958f0509b3319cf0936
asus <alikemalagirman@gmail.com>
2021-01-13 22:52:38 +0300

    updating our App to redirect to home if user sign in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 import Homepage from "./pages/homepage/homepage.component.jsx";
-import {Switch, Route} from "react-router-dom";
+import {Switch, Route, Redirect} from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx"
@@ -12,12 +12,7 @@ import {setCurrentUser} from "./redux/user/user.actions"
 
 
 class App extends React.Component {
-  
-
-
-
   unSubscribeFromAuth = null;
-
   componentDidMount() {
     const {setCurrentUser} = this.props;
     this.unSubscribeFromAuth= auth.onAuthStateChanged (async userAuth => {
@@ -44,7 +39,10 @@ class App extends React.Component {
         <Switch>
           <Route exact path = {"/"} component = {Homepage}/>
           <Route path = {"/shop"} component = {ShopPage}/> 
-          <Route path = {"/signin"} component = {SignInAndSignUp}/> 
+          <Route exact path = {"/signin"} render= {() => this.props.currentUser?(
+            <Redirect to ="/"/>)
+            : 
+            (<SignInAndSignUp/>)}/> 
         </Switch>
   
       </div>
@@ -52,9 +50,14 @@ class App extends React.Component {
   }
 }
 
+
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
 
   setCurrentUser: user=> dispatch(setCurrentUser(user))
 })
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
  
